28ca2a8a9346b18c4bfda4e2ee5df03e73df268a
fuadahammed101 <fuad.ahammed.cse@gmail.com>
2026-09-22 18:33:09 +0600

    -

--- voting.py
+++ voting.py
@@ -1,31 +1,38 @@
 #candidates list
 candidates = ["Pranta", "Fuad", "Nafis"] 
+
 #vote counter
 vote_count = {"Pranta": 0, "Fuad": 0, "Nafis": 0} 
 
-age = int(input("\n****** Welcome to Voting System ******\n\nEnter you age: "))
+while True:
+    age = int(input("\n****** Welcome to Voting System ******\n\nEnter you age: "))
 
-if age < 18:
-    print("\nYou are not eligible.\n")
-elif age >= 18:
-    print("\nChoose your candidate by their id: \n0: Pranta \n1: Fuad \n2: Nafis")
+    if 
 
-    vote = int(input("\nEnter vote: "))
+    #validate age
+    if age < 18:
+        print("\nYou are not eligible.\n")
+    elif age >= 18:
+        print("\nChoose your candidate by their id: \n0: Pranta \n1: Fuad \n2: Nafis")
 
-    if vote == 0:
-        print("\nVote casted for: ",candidates[0])
-        vote_count["Pranta"] += 1
-        print("\n",vote_count,"\n")
+        vote = int(input("\nEnter vote: "))
 
-    elif vote == 1:
-        print("\nVote casted for: ",candidates[1])
-        vote_count["Fuad"] += 1
-        print("\n",vote_count,"\n")
-    elif vote == 2:
-        print("\nVote casted for: ",candidates[2])
-        vote_count["Nafis"] += 1
-        print("\n",vote_count,"\n")
-    else:
-        print("Error!")
+    #cast vote
+        if vote == 0:
+            print("\nVote casted for: ",candidates[0])
+            #count vote
+            vote_count["Pranta"] += 1
+            print("\n",vote_count,"\n")
+
+        elif vote == 1:
+            print("\nVote casted for: ",candidates[1])
+            vote_count["Fuad"] += 1
+            print("\n",vote_count,"\n")
+        elif vote == 2:
+            print("\nVote casted for: ",candidates[2])
+            vote_count["Nafis"] += 1
+            print("\n",vote_count,"\n")
+        else:
+            print("Error!")
+        
     
-   
